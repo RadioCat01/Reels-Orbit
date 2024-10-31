473138c69d8acb62368b7082054e95ffa27deccd
RadioCat01 <pwnkanishka@gmail.com>
2024-10-31 19:21:49 +0530

    frontend COontainarized

--- Reels-Orbit-Web/vite.config.js
+++ Reels-Orbit-Web/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  /*server: {
     host: true,
     port: 5173,
     watch: {
       usePolling: true,
     },
-  },
+  },*/
 })
