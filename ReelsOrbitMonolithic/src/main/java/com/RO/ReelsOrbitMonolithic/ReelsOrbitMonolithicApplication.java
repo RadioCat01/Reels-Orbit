0d8cf5a2fcf749da2437c27b25bef6bf3e245c2c
Pawan Hettiarachchi <pwnkanishka@gmail.com>
2025-02-11 21:20:49 +0530

    Monolithic implementation starting

---
+++ ReelsOrbitMonolithic/src/main/java/com/RO/ReelsOrbitMonolithic/ReelsOrbitMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.RO.ReelsOrbitMonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelsOrbitMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelsOrbitMonolithicApplication.class, args);
+	}
+
+}
